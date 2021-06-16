8d324b5fb983752cba02761d5942675017e096ad
Luminatione <patryklazowski@hotmail.com>
2021-06-16 13:45:23 +0200

    Implemented ResposeReaderClass

--- ClientDownloadGuard/ResponseReader.cpp
+++ ClientDownloadGuard/ResponseReader.cpp
@@ -1,1 +1,9 @@
 #include "ResponseReader.h"
+
+#include <QNetworkReply>
+
+std::tuple<QJsonValueRef, QJsonValueRef> ResponseReader::GetStateAndValueJsonObjects(QNetworkReply* reply)
+{
+	QJsonDocument document = QJsonDocument::fromJson(reply->readAll());
+	return std::make_tuple(document.object()[stateName], document.object()[valueName]);
+}
--- ClientDownloadGuard/ResponseReader.h
+++ ClientDownloadGuard/ResponseReader.h
@@ -1,6 +1,15 @@
 #pragma once
+
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <qnetworkreply.h>
+#include <tuple>
+
 class ResponseReader
 {
-	
+	QString stateName = "state";
+	QString valueName = "value";
+public:
+	std::tuple<QJsonValueRef, QJsonValueRef> GetStateAndValueJsonObjects(QNetworkReply* reply);
 };
 
